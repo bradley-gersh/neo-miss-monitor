e9158ba000073a4fe0ff0866c8c1c2b8ae57e69d
Brad Gersh <bradley.gersh@gmail.com>
2021-01-23 17:25:44 -0500

    feat (index screen has title and options button)

--- components/IndexView.js
+++ components/IndexView.js
@@ -1,17 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 const background = require("../assets/background.png");
 
 export function IndexView() {
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.image} source={background}>
-        <Text style={styles.titleText}>HELLO</Text>
-        {/* <StatusBar barStyle="light-content" /> */}
+    <SafeAreaView style={styles.container}>
+      <ImageBackground style={styles.bgImage} source={background}>
+        <StatusBar style="light" />
+        <View style={styles.topBar}>
+          <Text style={styles.titleText}>neo misses</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Options</Text>
+          </TouchableOpacity>
+        </View>
       </ImageBackground>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -22,14 +34,35 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
   },
-  titleText: {
-    color: "#fff",
-    fontSize: 50,
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    // backgroundColor: "white",
   },
-  image: {
+  titleText: {
+    color: "#eeeeee",
+    fontSize: 48,
+    fontFamily: "System",
+    // backgroundColor: "pink",
+  },
+  bgImage: {
     flex: 1,
     resizeMode: "cover",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+  },
+  button: {
+    borderColor: "#eeeeee",
+    borderWidth: 3,
+    borderRadius: 10,
+    marginTop: 5,
+    // backgroundColor: "green",
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    padding: 6,
+    // backgroundColor: "red",
   },
   // text: {
   // color: "white",
